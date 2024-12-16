b6920e2b2a47dd8bc6705ffcd2a466b40e43ffdf
Tibor Claassen <tc@codebeige.net>
2024-12-16 19:59:44 +0100

    Fix setup

--- lazy.lua
+++ lazy.lua
@@ -1,8 +1,8 @@
 return {
   "codebeige/pilot.nvim",
+  config = true,
   dependencies = { "codebeige/rig.nvim" },
   ft = "fennel",
   lazy = true,
   main = "pilot",
-  opts = { fennel = "fennel" },
 }
